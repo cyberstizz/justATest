f0396e035e265bec27e8f00280eea20bd4c37b9b
Cyberstizz <charles.lamb.dev@gmail.com>
2023-03-21 22:53:09 -0400

    first commit

---
+++ justATest/src/main/java/com/example/justATest/JustATestApplication.java
@@ -0,0 +1,13 @@
+package com.example.justATest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustATestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustATestApplication.class, args);
+	}
+
+}
---
+++ justATest/src/test/java/com/example/justATest/JustATestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.justATest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustATestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
